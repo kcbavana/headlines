b68793a57f1f3c786aa14788b7a81b56a1920dd9
Dakota Eckheart <deckheart@cpp.edu>
2021-02-13 09:38:52 -0800

    Initial Django/React files

---
+++ backend/todo/serializers.py
@@ -0,0 +1,9 @@
+# todo/serializers.py
+
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Todo
+    fields = ('id', 'title', 'description', 'completed')
