9486d14a07e71def9a670912b1fdd8753e8da409
Jojo Chen <jojochen@berkeley.edu>
2021-02-13 17:48:37 -0800

    ixed home page

--- frontend/src/components/Default.js
+++ frontend/src/components/Default.js
@@ -1,47 +1,66 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import './style.css';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import styles from "./style.css";
 
 export default class Default extends Component {
-    constructor(props) {
-      super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="header">
-                <div className="container">
-                    <h3 className="title"><span className="accentText">H</span>eadline</h3>
-                    <div className="buttons">
-                    <h4 className="secondaryButton login"><Link to="/login">Log in</Link></h4>
-                    <h4 className="primaryButton signup"><ul className="current0"><a href="signup.html">Sign Up</a></ul></h4>
-                    </div>
-                </div>
-                </div>
-                <div className="hero">
-                <h1>Get the <span className="accentText mainCTA">Full Picture</span> on Daily Headlines</h1>
-                <h4>Stay up to date and make your perspective heard.</h4>
-                <h4 className="primaryButton mainCTA">Let's Go!</h4>
-                </div>
-                <div className="news">
-                <div className="today">
-                    <div className="titleLeft"><h3>Today's Headlines</h3></div>
-                    <div className="headlines">
-                    <h5 className="headlineCard">Sample Headline</h5>
-                    </div>
-                </div>
-                <div className="past">
-                    <h3 className="titleLeft">Past Headlines</h3>
-                    <div className="headlines">
-                    <h5 className="headlineCard">Sample Headline</h5>
-                    </div>
-                </div>
-                </div>
-                <div className="footer">
-                <h5>Tree Hacks 2021</h5>
-                </div>
+  render() {
+    return (
+      <div>
+        <div class="header">
+          <div class="container">
+            <h3 class="title">
+              <span class="accentText">H</span>eadline
+            </h3>
+            <div class="buttons">
+              <h4 class="secondaryButton login">
+                <a href="login.html">Log in</a>
+              </h4>
+              <h4 class="primaryButton signup">
+                <ul class="current0">
+                  <a href="signup.html">Sign Up</a>
+                </ul>
+              </h4>
             </div>
-        );
-    }
+          </div>
+        </div>
+
+        <div class="hero">
+          <h1>
+            Get the <span class="accentText">Full Picture</span> on Daily
+            Headlines
+          </h1>
+          <h4>Stay up to date and make your perspective heard.</h4>
+          <h4 class="primaryButton mainCTA">
+            <ul class="current0">
+              <a href="signup.html">Let's Go</a>
+            </ul>
+          </h4>
+        </div>
+        <div class="news">
+          <div class="today">
+            <div class="titleLeft">
+              <h3>Today's Headlines</h3>
+            </div>
+            <div class="headlines">
+              <h5 class="headlineCard">Sample Headline</h5>
+            </div>
+          </div>
+
+          <div class="past">
+            <h3 class="titleLeft">Past Headlines</h3>
+            <div class="headlines">
+              <h5 class="headlineCard">Sample Headline</h5>
+            </div>
+          </div>
+        </div>
+        <div class="footer">
+          <h5>Tree Hacks 2021</h5>
+        </div>
+      </div>
+    );
+  }
 }
