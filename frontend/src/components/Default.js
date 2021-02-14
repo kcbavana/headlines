b5b2d4b33a6c7dc9980d214805ed3db109186ca2
Jojo Chen <jojochen@berkeley.edu>
2021-02-13 19:50:05 -0800

    styling

--- frontend/src/components/Default.js
+++ frontend/src/components/Default.js
@@ -1,76 +1,57 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import styles from "./style.css";
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import './style.css';
 
 export default class Default extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+      super(props);
+    }
+    
+    
+    options = {
+        title: {
+            display:true,
+            text: '102 votes'
+        }
+    };
 
-  render() {
-    return (
-      <div>
-        <div class="header">
-          <div class="container">
-            <h3 class="title">
-              <span class="accentText">H</span>eadline
-            </h3>
-            <div class="buttons">
-              <Link to='/login'>
-                  <h4 class="secondaryButton login">
-                    Log in
-                </h4>
-                </Link>
-              <Link to='/signup'>
-                  <h4 class="primaryButton signup">
-                    <ul class="current0">
-                    Sign Up
-                    </ul>
-                </h4>
-                </Link>
+    render() {
+        return (
+            <div>
+                <div className="header">
+                <div className="container">
+                <Link to="/"><h2 className="title"><span className="accentText">H</span>eadline</h2></Link>
+                    <div className="buttons">
+                    <Link to="/login"><h4 className="secondaryButton login">Log in</h4></Link>
+                    <Link to="/signup"><h4 className="primaryButton signup">Sign Up</h4></Link>
+                    </div>
+                </div>
+               
+                </div>
+                <div className="hero">
+                <h1>Get the <span className="accentText ">Full Picture</span> on Daily Headlines</h1>
+                <h4>Stay up to date and make your perspective heard.</h4>
+                <h4 className="primaryButton mainCTA">Let's Go!</h4>
+                </div>
+                <div className="news">
+                <div className="today">
+                    <div className="titleLeft"><h3>Today's Headlines</h3></div>
+                    <div className="headlines">
+                    <Link to="/headlines"><h5 className="headlineCard">Sample Headline</h5></Link>
+                    
+                    </div>
+                </div>
+                <div className="past">
+                    <h3 className="titleLeft">Past Headlines</h3>
+                    <div className="headlines">
+                    <Link to="/headlines"><h5 className="headlineCard">Sample Headline</h5></Link>
+                    </div>
+                </div>
+                </div>
+                <div className="footer">
+                <h5>Tree Hacks 2021</h5>
+                </div>
             </div>
-          </div>
-        </div>
-
-        <div class="hero">
-          <h1>
-            Get the <span class="accentText">Full Picture</span> on Daily
-            Headlines
-          </h1>
-          <h4>Stay up to date and make your perspective heard.</h4>
-          <Link to='/signup'>
-          <h4 class="primaryButton mainCTA">
-            <ul class="current0">
-              Let's Go
-            </ul>
-          </h4>
-          </Link>
-        </div>
-        <div class="news">
-          <div class="today">
-            <div class="titleLeft">
-              <h3>Today's Headlines</h3>
-            </div>
-            <div class="headlines">
-                <Link to='/headlines'>
-                    <h4 class="headlineCard">Sample Headline</h4>
-                </Link>
-            </div>
-          </div>
-
-          <div class="past">
-            <h3 class="titleLeft">Past Headlines</h3>
-            <div class="headlines">
-                <Link to='/headlines'>
-                  <h4 class="headlineCard">Sample Headline</h4>
-                </Link>
-            </div>
-          </div>
-        </div>
-        <div class="footer">
-          <h5>Tree Hacks 2021</h5>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
--- frontend/src/components/Headline.js
+++ frontend/src/components/Headline.js
@@ -2,64 +2,100 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './style.css';
 
-export default class Headline extends Component {
-    state = {
-        listArticles: ["1", "2"]
-    }
+export default class Login extends Component {
     constructor(props) {
       super(props);
     }
 
     render() {
         return (
-            <div>
-                <div className="header">
+          <div style={{backgroundColor:"#F3F3F4"}}>
+            <div className="header">
                 <div className="container">
-                    <Link to='/'><h3 className="title"><span className="accentText">H</span>eadline</h3></Link>
+                  <Link to="/"><h2 className="title"><span className="accentText">H</span>eadline</h2></Link>
                     <div className="buttons">
-                    <Link to='/login'><h4 className="secondaryButton login">Log in</h4></Link>
-                    <Link to='/signup'><h4 className="primaryButton signup">Sign Up</h4></Link>
+                    <Link to="/login"><h4 className="secondaryButton login">Log in</h4></Link>
+                    <Link to="/signup"><h4 className="primaryButton signup">Sign Up</h4></Link>
                     </div>
                 </div>
                 </div>
-                <div className="articleTitle">
-                <Link to='/'><h4>Back to all Headlines.</h4></Link>
-                <React.Fragment>
-                        {this.state.listArticles.map(article => (
-                            <div className="sourceCard source1">{article}</div>
-                        ))}
-                </React.Fragment>
-                <h1>[Article Title] Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-                <p><b>Posted on January 1, 2021</b></p>
+
+        <div class="articleTitle">
+            <Link to="/"> <a href="default.html"><h4>Back to all Headlines.</h4></a></Link>
+            <h1>[Article Title] Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+            <p><b>Posted on January 1, 2021</b></p>
+        </div>
+        <div class="sources">
+            <div class="sourceCard source1">
+                <h4>New York Times</h4>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non.</p>
+            </div>
+            <div class="sourceCard source2">
+                <h4>Wall Street Journal</h4>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non.</p>
+            </div>
+
+        </div>
+
+        <div class="voting">
+            <h3>This is what I think...</h3>
+            <div class="votingButtons">
+                <h4 class="secondaryButton vote">Support</h4>
+                <h4 class="secondaryButton vote">Neutral</h4>
+                <h4 class="secondaryButton vote vote3">Disapprove</h4>
+            </div>
+            <canvas id="votingChart" width="600" height="200"></canvas>
+        </div>
+
+        <div class="commentSection">
+            <form action="" method="post" class="commentForm">
+                <input type="text" name="reply" id="reply" 
+                value="share your thoughts..." onfocus=" this.value='';"
+                rows="10" tabindex="1" required="required"/>
+                <h4 class="primaryButton commentButton">Comment</h4>
+            </form>
+            <div class="prevComments">
+                <div class="prevComment">
+                    <div class="commentContent">
+                        <p><b>Eric</b></p> 
+                        <div class="commentText">
+                            <p>This twitter thread is interesting and relevant: <a href="https://twitter.com">twitter.com</a></p>
+                        </div>
+                        <div class="upDownVote">
+                            <div class="arrows">
+                                <img src="images/upArrowGray.png" width="20"/>
+                                <img class="bottomArrow" src="images/downArrowGray.png" width="20"/>
+                            </div>
+                            <h3 class="score">25</h3>
+                        </div>
+                    </div>
+                    <hr/>
                 </div>
-                <div className="sources">
-                <div className="sourceCard source1">
-                    <h4>New York Times</h4>
-                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non.</p>
-                </div>
-                <div className="sourceCard source2">
-                    <h4>Wall Street Journal</h4>
-                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non.</p>
-                </div>
-                </div>
-                <div className="voting">
-                <h3>This is what I think...</h3>
-                <div className="votingButtons">
-                    <h4 className="secondaryButton vote">Support</h4>
-                    <h4 className="secondaryButton vote">Neutral</h4>
-                    <h4 className="secondaryButton vote">Disapprove</h4>
-                </div>
-                <canvas id="votingChart" width={600} height={200} />
-                </div>
-                <div className="articleTitle">
-                <h4>Back to all Headlines.</h4>
-                <h1>[Article Title] Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
-                <p><b>Posted on January 1, 2021</b></p>
-                </div>
-                <div className="footer">
-                <h5>Tree Hacks 2021</h5>
+                <div class="prevComment">
+                    <div class="commentContent">
+                        <p><b>Eric</b></p> 
+                        <div class="commentText">
+                            <p>This twitter thread is interesting and relevant: <a href="https://twitter.com">twitter.com</a></p>
+                        </div>
+                        <div class="upDownVote">
+                            <div class="arrows">
+                                <img src="images/upArrowGray.png" width="20"/>
+                                <img class="bottomArrow" src="images/downArrowGray.png" width="20"/>
+                            </div>
+                            <h3 class="score">25</h3>
+                        </div>
+                    </div>
+                    <hr/>
                 </div>
             </div>
+            
+            
+        </div>
+        
+        <div class="footer">
+            <h5>Tree Hacks 2021</h5>
+        </div>
+          </div>
         );
     }
 }
